02c4181a719f0794e6c9156831608b2628ad4af5
Mary-Anne Triggs <maryannetriggs@gmail.com>
2020-03-09 15:31:25 +0000

    begun styling

--- src/app.js
+++ src/app.js
@@ -32,49 +32,51 @@ class App extends React.Component {
 
   getVideo() {
     const youtubeKey = process.env.YOUTUBE_API_KEY
-    axios.get(`https://cors-anywhere.herokuapp.com/https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=1&q=${this.state.search}&key=${youtubeKey}`, {
-      headers: {
-        Accept: 'application/json'
-      }
-    })
+    axios.get(`https://cors-anywhere.herokuapp.com/https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=1&q=${this.state.search}&key=${youtubeKey}`)
       .then(res => this.setState({ video: res.data.items[0].id.videoId }))
       .catch(err => console.log(err))
   }
 
   getPhotos() {
     const flickrKey = process.env.FLICKR_API_KEY
-    console.log(this.state.search)
     axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${flickrKey}&tags=${this.state.search}&format=json&nojsoncallback=1`)
       .then(res => this.setState({ photos: res.data.photos.photo }))
       .catch(err => console.log(err))
   }
 
   render() {
-    const { photos, video } = this.state
+    const { photos, video, search } = this.state
     console.log(this.state)
     return (
-      <>
-        <h1>Search Return</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Your Search:
-            <input type="text" value={this.state.search} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
+      <div className="site-wrapper">
+        <section className="">
+          <h1 className="title is-1">Search yourself happy!</h1>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+            What do you want to see? 
+              <br/>
+              <input placeholder="Type Here" type="text" value={this.state.search} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Search!" />
+          </form>
+        </section>
+        
+        <section className="video-wrapper">
+          <iframe width="640" height="480" controls src={`https://www.youtube.com/embed/${video}`}>Your browser does not support this video type</iframe>
+        </section>
 
-        <iframe width="320" height="240" controls src={`https://www.youtube.com/embed/${video}`}/>
-
-        {this.state.photos ?
-          <div>
-            {photos.slice(0, 3).map(photo => (
-              <img key={photo.id} src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} alt="REMEMBER" height="500" width="500"></img>
-            ))}
-          </div>
-          :
-          <h1>Image Pending</h1>
-        }
-      </>
+        <section className="photo-wrapper">
+          {this.state.photos ?
+            <div>
+              {photos.slice(0, 3).map(photo => (
+                <img key={photo.id} src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} alt={`${search}images`} height="500" width="500"></img>
+              ))}
+            </div>
+            :
+            <h3 className="title is-3">Your search images will appear here...</h3>
+          }
+        </section>
+      </div>
     )
   }
 }
